cd26505c8b4abc0172846c7099046de337377bff
ming <fox20431@gmail.com>
2023-03-01 20:17:06 +0800

    use cmake replacing to vscode

--- algorithms/java/huawei-od/HJ1.java
+++ algorithms/java/huawei-od/HJ1.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 
-// 注意类名必须为 Main, 不要有任何 package xxx 信息
-public class Main {
+public class HJ1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.nextLine();
-        String[] s = str.split(" "); // 正则表达式实用性更强( str.split("\\s+"))
+        String[] s = str.split(" ");
+        // 正则表达式实用性更强
+        // String[] s = str.split("\\s+");
         int length = s[s.length - 1].length();
         System.out.println(length);
     }
